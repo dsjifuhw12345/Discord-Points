768b4e4450bdaf29228a8f47fd0b74980ae1a432
dsjifuhw12345 <113939431+dsjifuhw12345@users.noreply.github.com>
2024-11-30 21:52:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-clientId: "", // ايدي البوت
-    guildId: "", // ايدي السيرفر
-    token: "", // توكن البوت
-    CommandPermissions: "" // ايدي الرتبة الى تقدر تستخدم الامر
+clientId: "1310439597255426138", // ايدي البوت
+    guildId: "1279444720216445051", // ايدي السيرفر
+    token: "MTMxMDQzOTU5NzI1NTQyNjEzOA.G1uBAH.G3J-k9QBIAj6bqL7pqzTL-O0rPPjANUtZedrnw", // توكن البوت
+    CommandPermissions: "1307722865507958824" // ايدي الرتبة الى تقدر تستخدم الامر
 }
